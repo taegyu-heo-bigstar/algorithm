985a91404de413e3af17a7bd9b97a3faeb205102
taegyu-heo <taegyu-heo@naver.com>
2025-04-06 01:44:45 +0900

    Update example_case.java

--- sort/example_case.java
+++ sort/example_case.java
@@ -2,6 +2,16 @@ packeage sort;
 
     public class example_case{
         public static void class main(String[] args){
-           System.out.print("hello world");
-        }
+           
+            Scanner sc = new Scanner(System.in);
+            System.out.println("입력 파일 이름? ");
+            String fname = sc.nextLine();
+            sc.close();
+            
+            try {
+                sc = new Scanner(new File(fname));
+                int input_data = sc.nextInt();  // file에서 정수를 읽어 input_data에 저장
+                                                // 이후 sc.nextInt()를 이용하여 계속 정수 입력} catch (IOException e) { System.out.println(e); return; }
+                if (sc != null) sc.close();
+            }
     }
