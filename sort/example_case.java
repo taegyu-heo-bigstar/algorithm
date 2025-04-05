3876691979bf80b47f24815568077862cecf0ae1
taegyu-heo <taegyu-heo@naver.com>
2025-04-06 02:55:08 +0900

    Update example_case.java

--- sort/example_case.java
+++ sort/example_case.java
@@ -1,17 +1,57 @@
 packeage sort;
 
-    public class example_case{
-        public static void class main(String[] args){
-           
-            Scanner sc = new Scanner(System.in);
-            System.out.println("입력 파일 이름? ");
-            String fname = sc.nextLine();
-            sc.close();
-            
-            try {
-                sc = new Scanner(new File(fname));
-                int input_data = sc.nextInt();  // file에서 정수를 읽어 input_data에 저장
-                                                // 이후 sc.nextInt()를 이용하여 계속 정수 입력} catch (IOException e) { System.out.println(e); return; }
-                if (sc != null) sc.close();
+public class example_case{
+
+    public class Point<X, Y> {
+        public final X x;
+        public final Y y;
+
+        public Point(X x, Y y) {
+            this.x = x;
+            this.y = y;
             }
     }
+
+    public static double cal_dis(Point origin, Point[] Points){
+        
+    }
+
+    public static void class main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("입력 파일 이름? ");
+        String fname = sc.nextLine();
+        System.out.println("");
+        sc.close();
+        
+        try {
+            sc = new Scanner(new File(fname));
+
+            double num1 = sc.nextDouble();
+            double num2 = sc.nextDouble();
+
+            Point origin = new Point(num1, num2);
+            int k = sc.nextInt();
+            int n = sc.nextInt();
+
+            Point[] point_arr = new Point[n];
+            for (int i = 0; i < n; i++){
+                while (sc.hasNextDouble()) {
+                    num1 = sc.nextDouble();
+                    num2 = sc.nextDouble();
+                }
+                point_arr[i] = new point(num1, num2); 
+            }
+        }
+        catch (IOException e) { System.out.println(e); return; }
+
+        
+
+        System.out.print("k = " + {k} + "일 때의 실행시간 = ");
+
+
+        
+        if (sc != null) sc.close();
+        
+    }
+}
