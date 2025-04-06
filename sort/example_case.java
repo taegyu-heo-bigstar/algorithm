ea485bae40601ec22f9e06d778029ad545d02e40
taegyu-heo <taegyu-heo@naver.com>
2025-04-06 11:12:18 +0900

    Update example_case.java

--- sort/example_case.java
+++ sort/example_case.java
@@ -2,28 +2,35 @@ packeage sort;
 
 public class example_case{
 
-    public class Point<X, Y> {
-        public final X x;
-        public final Y y;
+    public class Point {
+        public final double x;
+        public final double y;
 
-        public Point(X x, Y y) {
+        public Point(double x, double y) {
             this.x = x;
             this.y = y;
-            }
+        }
+
+        public double distanceTo(Point other) {
+            double dx = this.x - other.x;
+            double dy = this.y - other.y;
+            
+            return Math.sqrt(dx * dx + dy * dy);
+        }
     }
 
-    public static double cal_dis(Point origin, Point[] Points){
-        
-    }
 
     public static void class main(String[] args){
         
+        //input file name
         Scanner sc = new Scanner(System.in);
         System.out.print("입력 파일 이름? ");
         String fname = sc.nextLine();
         System.out.println("");
         sc.close();
-        
+
+        //read file
+        //create origin, k, n and another Points
         try {
             sc = new Scanner(new File(fname));
 
@@ -40,12 +47,16 @@ public class example_case{
                     num1 = sc.nextDouble();
                     num2 = sc.nextDouble();
                 }
-                point_arr[i] = new point(num1, num2); 
+                point_arr[i] = new Point(num1, num2); 
             }
         }
         catch (IOException e) { System.out.println(e); return; }
 
-        
+        //calculate distance origin from other Point
+        double[] dis_arr = new double[n];
+        for (int i = 0; i<n; i++){
+            dis_arr[i] = origin.distanceTo(point_arr[i])
+        }    
 
         System.out.print("k = " + {k} + "일 때의 실행시간 = ");
 
