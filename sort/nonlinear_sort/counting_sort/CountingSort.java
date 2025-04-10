787cf17b5ae10803fe110b1ae97d7df49d5e3634
taegyu-heo <taegyu-heo@naver.com>
2025-04-10 16:51:08 +0900

    Update CountingSort.java

--- sort/nonlinear_sort/counting_sort/CountingSort.java
+++ sort/nonlinear_sort/counting_sort/CountingSort.java
@@ -11,7 +11,7 @@ public class CountingSort {c
         return B;
     }
 
-    public static void my_sort(int[] A, int K){
+    public static void nonReturn_sort(int[] A, int K){
         int i, N = A.length;
         int[] C = new int[K], B = new int[N];
     
@@ -19,6 +19,6 @@ public class CountingSort {c
         for (i = 1; i < K; i++) C[i] += C[i-1]; // i 값이 저장될 위치를 계산
         for (i = N-1; i >= 0; i--) B[--C[A[i]]] = A[i]; // 정렬된 배열을 생성
     
-        A = B;
+        for (i = 0; i < N; i++) A[i] = B[i];
     }
 } 
