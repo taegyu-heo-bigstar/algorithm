c6e2f1c0eae2e250b99355df6b6a249fb5f8239d
taegyu-heo <taegyu-heo@naver.com>
2025-04-10 14:57:43 +0900

    Update ShellSort.java

--- sort/linear_sort/shell_sort/ShellSort.java
+++ sort/linear_sort/shell_sort/ShellSort.java
@@ -1,4 +1,5 @@
 public class ShellSort extends AbstractSort{
+    //generally usde h-sequence
     public static void sort(Comparable[] a){
         int N = a.length;
         int h = 1;
@@ -14,20 +15,21 @@ public class ShellSort extends AbstractSort{
         }
     }
     
-    //this function is for the test
+    //user define h-sequence
     public static void sort(Comparable[] a, int[] h_seq){
         if (h_seq[0] != 1) return;
         
-        int N = a.length;
+        int N = a.length;    if (h_seq[N-1] > N) return;
         int idx = h_seq.length;
         int h = h_seq[idx];
         
-        while(h >= 1){
+        while(true){
             for (int i = 0; i < N; i++){
                 for (int j = i; j > h && less(a[j], a[j-h]); j-=h){
                     exch(a, j, j-h);
                 }
             }
+            if (idx == 0) break;
             h = h_seq[--idx];
         }            
     }
